d4d146a22aa5ad3362780ae618dbf306f55965e5
andrebeu <aobeukers@gmail.com>
2020-10-14 09:39:18 -0400

    timing file generated; instead of having a dataframe created within the notebook, I made a notebook for extracting onset and offset TR for each wedding

--- utils.py
+++ utils.py
@@ -11,8 +11,8 @@ import nilearn as nl
 from nilearn import image, plotting, input_data
 from scipy.spatial import distance
 
-# rm sub_ns = [2,11]
-ALL_SUB_NS = [3,4,5,6,7,8,9,10,12,13,14,15,17,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44]
+# rm sub_ns = [2,11,39]
+ALL_SUB_NS = [3,4,5,6,7,8,9,10,12,13,14,15,17,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,40,41,42,43,44]
 SUB_NS = np.arange(30,39)
 WED_L = ['W1', 'W2', 'W6', 'W17', 'W19', 'W20', 'W22', 'W23', 'W28', 'W29', 'W34', 'W38']
 N_WEDDINGS = 12
