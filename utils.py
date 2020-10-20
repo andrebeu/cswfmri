d9317ed57b8c67351e5aa48bbb1bc7d45f0d5542
andrebeu <aobeukers@gmail.com>
2020-10-20 19:55:53 -0400

    git cache cleared

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 
-""" """
+""" loading utilities """
 
 import numpy as np
 import pandas as pd
@@ -12,7 +12,13 @@ from nilearn import image, plotting, input_data
 from scipy.spatial import distance
 
 # rm sub_ns = [2,11,37,39]
-ALL_SUB_NS = [3,4,5,6,7,8,9,10,12,13,14,15,17,18,19,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,38,40,41,42,43,44]
+ALL_SUB_NS = [
+  3,4,5,6,7,8,9,10,
+  12,13,14,15,17,18,19,
+  22,23,24,25,26,27,28,29,
+  30,31,32,33,34,35,36,38,
+  40,41,42,43,44
+  ]
 SUB_NS = [30,31,32,33,34,35,36,38]
 WED_L = ['W1', 'W2', 'W6', 'W17', 'W19', 'W20', 'W22', 'W23', 'W28', 'W29', 'W34', 'W38']
 N_WEDDINGS = 12
@@ -22,14 +28,12 @@ ROI_NAME_L = [
   'rglasser_MTN_net',
   'rglasser_PM_net',
   'rhippocampusAAL',
-  # 'rsherlockAvg_fc_thr5_mpfc',
+  'rsherlockAvg_fc_thr5_mpfc',
   # 'rsherlockAvg_fc_thr5_pmc',
   # 'rsherlockAvg_fc_thr5_lTPJ',
   # 'rsherlockAvg_fc_thr5_rTPJ',
   # 'rsherlockAvg_fc_thr5_lSFG',
   # 'rsherlockAvg_fc_thr5_rSFG',
-  # 'rhippocampusL_AAL',
-  # 'rhippocampusR_AAL'  
   ]
 
 def load_sub4d(sub_n,task='videos',max_len=2000,numpy_output=False):
@@ -37,7 +41,7 @@ def load_sub4d(sub_n,task='videos',max_len=2000,numpy_output=False):
   task = videos / recall2 
   """
   ses = 2
-  fpath = 'fmri_data/func/sub-1%i_ses-0%i_task-%s_space-MNI152NLin2009cAsym_desc-preproc_bold.nii'%(sub_n,ses,task)
+  fpath = 'fmri_data/func/sub-1%i_ses-0%i_task-%s_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'%(sub_n,ses,task)
   
   try:
     img = nl.image.load_img(fpath)
