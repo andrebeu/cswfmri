50de64279689fc8631823e32cae35be451553eae
andrebeu <aobeukers@gmail.com>
2020-10-21 09:46:40 -0400

    phasing out utils.py

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ import nilearn as nl
 from nilearn import image, plotting, input_data
 from scipy.spatial import distance
 
-# rm sub_ns = [2,11,37,39]
+
 ALL_SUB_NS = [
   3,4,5,6,7,8,9,10,
   12,13,14,15,17,18,19,
@@ -21,7 +21,6 @@ ALL_SUB_NS = [
   ]
 SUB_NS = [30,31,32,33,34,35,36,38]
 WED_L = ['W1', 'W2', 'W6', 'W17', 'W19', 'W20', 'W22', 'W23', 'W28', 'W29', 'W34', 'W38']
-N_WEDDINGS = 12
 ROI_NAME_L = [
   'rglasser_AT_net',
   'rglasser_MP_net',
@@ -35,23 +34,6 @@ ROI_NAME_L = [
   # 'rsherlockAvg_fc_thr5_lSFG',
   # 'rsherlockAvg_fc_thr5_rSFG',
   ]
-
-def load_sub4d(sub_n,task='videos',max_len=2000,numpy_output=False):
-  """ 
-  task = videos / recall2 
-  """
-  ses = 2
-  fpath = 'fmri_data/func/sub-1%i_ses-0%i_task-%s_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'%(sub_n,ses,task)
-  
-  try:
-    img = nl.image.load_img(fpath)
-  except:
-    print('NOT FOUND:',fpath)
-    return None
-  img = img.slicer[:,:,:,:max_len] # lengths differ
-  if numpy_output:
-    img = img.get_fdata() # nilearn into np
-  return img
 
 
 def load_subj_behav_df(sub_n):
